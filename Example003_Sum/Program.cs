2b965c9ccdd44601bf5f85d49f5fa905ac1c3fb5
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-01-23 19:24:31 +0500

    new random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.Clear();
-int NumberA = 3;
-int NumberB = 2;
+int NumberA = new Random().Next(1,10); // 1,2,3...9
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1,10);
+Console.WriteLine(NumberB);
 int result = NumberA + NumberB;
 Console.WriteLine(result);
 
